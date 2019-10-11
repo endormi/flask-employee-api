6dd6aae0cbe47e07143446561f464e9764e4016e
endormi <39559256+endormi@users.noreply.github.com>
2019-10-11 12:30:44 +0300

    Add create_employee and get_employees def
    * Also rename Job class to Job_tasks
    
    * Add Werkzeug to requirements
    
    * Add simple ins (more to be added)

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Flask-Employee-API
 
-Simple API for Employees to keep track of job tasks.
+Simple API for Employees to keep track of job tasks with user authorization.
+
+```sh
+pip install -r requirements.txt
+```
+
+My `config.py` file is inside .gitignore, so you have to create `config` file manually.
+
+Example:
+
+```python
+import os
+
+class DevConfig(object):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///C:\\users\\endormi\\employee.db"
+    DEBUG = True
+    SECRET_KEY = ''
+```
--- api.py
+++ api.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash, generate_password_hash
+import uuid
 
 
 app = Flask(__name__)
@@ -19,11 +21,62 @@ class Employee(db.Model):
     password = db.Column(db.String(50))
 
 
-class Job(db.Model):
+class Job_tasks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task = db.Column(db.String(50))
     done = db.Column(db.Boolean)
     employee_id = db.Column(db.Integer)
+
+
+@app.route('/employee', methods=['GET'])
+def get_employees():
+    employees = Employee.query.all()
+
+    output = []
+
+    for employee in employees:
+
+        empl_data = {}
+
+        empl_data['public_id'] = employee.public_id
+        empl_data['name'] = employee.name
+        empl_data['public_id'] = employee.public_id
+        empl_data['job_title'] = employee.job_title
+        empl_data['password'] = employee.password
+        empl_data['admin'] = employee.admin
+
+        output.append(empl_data)
+
+    return jsonify({'Employee list': output})
+
+
+@app.route('/employee/<public_id>', methods=['GET'])
+def get_employee():
+    return ''
+
+
+@app.route('/employee', methods=['POST'])
+def create_employee():
+    employee_data = request.get_json()
+
+    pw_hash = generate_password_hash(employee_data['password'], method='sha256')
+
+    new_employee = Employee(public_id=str(uuid.uuid1()), name=employee_data['name'], job_title=employee_data['job_title'], password=pw_hash, admin=False)
+
+    db.session.add(new_employee)
+    db.session.commit()
+
+    return jsonify({'message': 'New employee added'})
+
+
+@app.route('/employee/<public_id>', methods=['PUT'])
+def promote_employee():
+    return ''
+
+
+@app.route('/employee/<public_id>', methods=['DELETE'])
+def remove_employee():
+    return ''
 
 
 if __name__ == '__main__':
